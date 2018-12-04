64ae5344ab5de8a2392156167017c3d2ca1c2951
Amit-A <kumar.amit@live.ca>
2018-12-04 15:33:58 -0500

    Update travis_deploy.sh

--- scripts/travis_deploy.sh
+++ scripts/travis_deploy.sh
@@ -3,21 +3,13 @@ set -e
 
 echo "* * * * *
 
-  Installing dependencies...
-
-* * * * *"
-
-npm install
-
-echo "* * * * *
-
   Building, tagging, and pushing the docker image...
 
 * * * * *"
 
 docker login -u $DOCKER_ID -p $DOCKER_PASSWORD
 docker build . --tag $DOCKER_ID/aws-es-kibana:$TRAVIS_COMMIT --tag $DOCKER_ID/aws-es-kibana:latest --compress
-docker push $DOCKER_ID/aws-es-kibana:$TRAVIS_COMMIT
+docker push $DOCKER_ID/aws-es-kibana:latest
 
 echo "* * * * *
 
